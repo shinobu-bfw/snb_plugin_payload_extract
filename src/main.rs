67486decae794d9fff1839d522108d3ad233bb5a
Kmiit <kmiit@foxmail.com>
2026-04-11 17:57:37 +0800

    log unhandled msg

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ mod tool;
 mod utils;
 
 use crate::commands::{Command, answer};
+use crate::utils::log_message;
 use anyhow::Result;
-use log::info;
+use log::{debug, info};
 use std::time::Duration;
 use teloxide::dispatching::{Dispatcher, HandlerExt, UpdateFilterExt};
 use teloxide::prelude::{Bot, Update};
@@ -28,10 +29,14 @@ async fn main() -> Result<()> {
         Bot::with_client(cfg.token.clone(), client.build()?).set_api_url(cfg.api_url.parse()?);
 
     let handler = Update::filter_message()
-        .branch(dptree::entry().filter_command::<Command>().endpoint(answer));
+        .branch(dptree::entry().filter_command::<Command>().endpoint(answer))
+        .branch(dptree::endpoint(log_message));
 
     Dispatcher::builder(bot, handler)
         .dependencies(dptree::deps![tm, cfg])
+        .default_handler(|upd| async move {
+            debug!("Ignoring unmatched update: {:?}", upd);
+        })
         .enable_ctrlc_handler()
         .build()
         .dispatch()
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,6 @@
+use teloxide::prelude::{Message, ResponseResult};
+use log::info;
+
 pub const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
 
 pub fn to_tg_md(s: String) -> String {
@@ -7,4 +10,26 @@ pub fn to_tg_md(s: String) -> String {
         .replace(")", "\\)")
         .replace("+", "\\+")
         .replace("#", "\\#")
+}
+
+pub async fn log_message(msg: Message) -> ResponseResult<()> {
+    let sender = msg
+        .from
+        .as_ref()
+        .map(|user| user.full_name())
+        .unwrap_or_else(|| "Unknown".to_string());
+    let sender_id = msg
+        .from
+        .as_ref()
+        .map(|user| user.id.0.to_string())
+        .unwrap_or_else(|| "unknown".to_string());
+    let group_name = msg.chat.title().unwrap_or("Private");
+    let group_id = msg.chat.id;
+
+    if let Some(text) = msg.text() {
+        info!("{sender}({sender_id}) [{group_name}({group_id})] : {text}");
+    } else {
+        info!("{sender}({sender_id}) [{group_name}({group_id})] : <non-text message>");
+    }
+    Ok(())
 }
